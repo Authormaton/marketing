5776645d4425aa04c1c4d88ce945fe92e96ed532
Jason Petro <jasonpetro865@gmail.com>
2025-10-24 03:05:47 +0000

    feat: Add classNames and formatYear helpers to utils

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,11 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function classNames(...args: ClassValue[]) {
+  return cn(...args)
+}
+
+export function formatYear() {
+  return new Date().getFullYear()
+}
