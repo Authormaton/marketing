2aa522c3fe328196266d7a0ca252c444a8f3c3ee
Akhun Akhand <akhun.web3@gmail.com>
2025-11-26 23:12:27 +0000

    Fix JSX parsing error in FormInput component

--- src/components/ui/FormInput.tsx
+++ src/components/ui/FormInput.tsx
@@ -70,14 +70,3 @@ export const FormInput = React.forwardRef<HTMLInputElement, FormInputProps>(
 });
 
 FormInput.displayName = 'FormInput';
-        />
-        {showCharCount && maxLength && (
-          <p className={cn("text-xs text-right", getCharCountColorClass())}>
-            {currentLength}/{maxLength} characters
-          </p>
-        )}
-        {hasError && <p id={`${id}-error`} role="alert" className="text-red-500 text-xs mt-1">{error}</p>}
-      </div>
-    );
-  }
-);
